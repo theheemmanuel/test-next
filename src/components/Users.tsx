4cc107e6d718ae45478ec7f0e2fe9730385d35ce
theheemmanuel <oladirane@gmail.com>
2024-11-23 08:04:16 +0100

    api integration

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -6,7 +6,11 @@ interface Users {
 }
 
 const Users = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+  const res = await fetch(process.env.NEXT_PUBLIC_SUPABASE_URL!, {
+    headers: {
+      apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`,
+    },
     next: { revalidate: 10 },
     // cache: "no-store",
   });
