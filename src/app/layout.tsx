31bbcebc7ff504251185fb51dffb9d41c72ef4b4
theheemmanuel <oladirane@gmail.com>
2024-11-30 16:39:36 +0100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 // import { Suspense } from "react";
 // import Loading from "./loading";
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
